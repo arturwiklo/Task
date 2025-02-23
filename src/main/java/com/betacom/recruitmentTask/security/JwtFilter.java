90751bea56de7bcc874dab1b11f3ea35ef272675
Artur <artur.wiklo@gmail.com>
2025-02-23 17:56:03 +0100

    removing unnecessary GlobalExceptionHandler

--- src/main/java/com/betacom/recruitmentTask/security/JwtFilter.java
+++ src/main/java/com/betacom/recruitmentTask/security/JwtFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -25,12 +24,13 @@ public class JwtFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
 
     public JwtFilter(JwtUtil jwtUtil, UserRepository userRepository) {
+
         this.jwtUtil = jwtUtil;
         this.userRepository = userRepository;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull  FilterChain filterChain)
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
             throws ServletException, IOException {
 
         String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
